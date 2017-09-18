8d579152ca89b0d02c473d8855119a75f499cd6d
Justin Garavel <xxjeep@gmail.com>
2017-09-18 12:47:41 +0900

    first hello world. (not using babel but raw React calls)

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+console.log('App JS is running!');
+
+// var template = <p> This is JSX from app.js</p>; 
+var template = React.createElement(
+    "h1",
+    null,
+    "This is JSX from app.js!"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
