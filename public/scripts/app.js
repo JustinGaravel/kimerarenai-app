edbd59d5a6a5ead36eb2486304a6ccdfbba1394c
Justin Garavel <xxjeep@gmail.com>
2017-09-20 12:10:25 +0900

    built count app to work on adding function to react dom elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,92 @@
-"use strict";
+'use strict';
 
-var multiplier = {
-  // array of numbers to multiple
-  // single num = multiply by
-  // multiply method - return new array w numbers mutiplied
-  numbers: [10, 20, 30],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this = this;
+console.log('App JS is running!');
 
-    return this.numbers.map(function (number) {
-      return number * _this.multiplyBy;
-    });
-  }
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is the subtitle',
+  options: ['One', 'Two']
 };
 
-console.log(multiplier.multiply());
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'There are no options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item Two'
+    )
+  )
+);
+
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
+};
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+};
+
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+};
+
+var appRoot = document.getElementById('app');
+
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
